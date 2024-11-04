aece07b3c248eda86aa5dab8aa5748545e6ac15b
anoBrain777 <horehura12@gmail.com>
2024-11-05 01:52:13 +0700

    edit dan update bab 8

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $data = Product::all();
+        return view("master-data.product-master.index-product",compact('data'));
     }
 
     /**
@@ -58,7 +59,8 @@ class ProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('master-data.product-master.edit-product',compact('product'));
     }
 
     /**
@@ -66,7 +68,26 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'product_name' => 'required|string|max:255',
+            'unit' => 'required|string|max:255',
+            'type' => 'required|string|max:255',
+            'information' => 'nullable|string',
+            'qty' => 'required|integer|min:1',
+            'producer' => 'required|string|max:255',
+        ]);
+
+        $product = Product::findOrFail($id);
+        $product->update ([
+            'product_name' => $request -> product_name,
+            'unit' => $request->unit,
+            'type' => $request->type,
+            'information' => $request->information,
+            'qty' => $request->qty,
+            'producer' => $request->producer,
+        ]);
+
+        return redirect()->back()->with('succes', 'product update succesfully');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,23 @@ use App\Http\Controllers\SupplierController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/product', [ProductController::class, 'index']);
-Route::get('/product/create', [ProductController::class, 'create']);
-Route::post('/product', [ProductController::class, 'store']);
-Route::get('/product/{id}', [ProductController::class, 'show']);
-Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
-Route::put('/product/{id}', [ProductController::class, 'update']);
-Route::delete('/product/{id}', [ProductController::class, 'destroy']);
-Route::get('/product/create', [ProductController::class, 'create'])->name('product-create');
-Route::post('/product', [ProductController::class, 'store'])->name('product-store');
-Route::get('/suppliers/create', [SupplierController::class, 'create'])->name('supplier-create');
-Route::post('/suppliers', [SupplierController::class, 'store'])->name('supplier-store');
+// SUPPLIER ROUTES
+Route::middleware(['auth', 'RoleCheck:admin'])->group(function () {
+    Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier.index');
+    Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
+    Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+});
 
+// PRODUCTS ROUTES
+Route::middleware(['auth', 'RoleCheck:admin'])->group(function () {
+    Route::get('/product', [ProductController::class, 'index'])->name("product-index");
+    Route::get('/product/create', [ProductController::class, 'create'])->name("product-create");
+    Route::post('/product', [ProductController::class, 'store'])->name("product-store");
+    Route::get('/product/{id}', [ProductController::class, 'show']);
+    Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name("product-edit");
+    Route::put('/product/{id}', [ProductController::class, 'update'])->name("product-update");
+    Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+});
 
 Route::get('/', function () {
     return view('welcome');
